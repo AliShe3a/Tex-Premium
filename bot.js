c305ec56b4bcea23ac87758c6785fa7725a70109
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-12 00:17:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -541,7 +541,7 @@ client.on('message', message => {
       if (!message.content.startsWith(prefix)) return;
       var args = message.content.split(' ').slice(1);
       var argresult = args.join(' ');
-      if (message.author.id == 357961207019470851) return;
+      if (message.author.id == "357961207019470851") return;
     
     if (message.content.startsWith(prefix + 'playing')) {
       client.user.setGame(argresult);
