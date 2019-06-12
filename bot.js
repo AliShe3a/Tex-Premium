9ece331fc968f97b8b2a8927800b4b145c70be7e
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-12 13:06:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,116 +6,38 @@ const Enmap = require('enmap');
 const ytScraper = require("yt-scraper")
 const prefix = "@"
 const config = require('./config.json');
-var database = {};// she3a
-// يا رجال , خذت سراويلي , قالت معلسش كنت احسبه لوت (:
-// lol wut u looking at ? launch this fuckincode!
-// she3a
-client.login(config.token).then(async function() {
-   console.log(`im ready at ${client.user.username}`);// she3a
-   console.log('Developed by Finnin#7214');
-});// she3a
+const account = JSON.parse(fs.readFileSync('./account.json', 'utf8')); // create " account.json " folder and put into it " {} "
+
 client.on('message', async message => {
-    // i said , Best Arabic Developer is she3a .
-   if (!message.content.startsWith(config.prefix + "register")) return undefined;
-   if (database[message.author.id]) return message.channel.send(`You have data already please try ${config.prefix}idenity`);
-   var xfilter = (reaction, user) => {
-                return reaction.emoji.name === `❎` && user.id === message.author.id;// she3a
-            };
-   var tfilter = (reaction, user) => {
-                return reaction.emoji.name === `✅` && user.id === message.author.id;// bitchhh stop ! u clearing my copyrights! thats not good!
-            };
-   var args = message.content.split(" ").slice(1);
-   var name = args[0];
-   var age = args[1];// she3a
-   var country = args[2];
-   if (!name || !age || !country) return message.channel.send({embed: new Discord.RichEmbed()
-       .setColor("#36393e")
-       .setDescription(`**Example; \`${config.prefix}register [name] [age] [country]\`**`)// why smartuteam devs better than arabic devs? u didn't know nah .
-   });
-   var embed = new Discord.RichEmbed()
-   .setColor("#36393e")// she3a
-   .setTimestamp()
-   .setAuthor(`${message.author.username} Apply;`,message.author.displayAvatarURL)
-   .setDescription(`\`\`\`apache\nName; ${name}\nAge; ${age}\nCountry; ${country}\`\`\``);// she3a
-   var awaited = await message.channel.send(embed);
-   await awaited.react("✅");
-   await awaited.react("❎");// she3a
-   var truecoll = awaited.createReactionCollector(tfilter, {
-                max: 1
-            });
-   var xcoll = awaited.createReactionCollector(xfilter, {
-                max: 1
-            });
-    xcoll.on('collect', async resp => {// she3a
-      does("nope");
-        
-    });
-    truecoll.on('collect', async resp => {
-    does("yes");
-    });
-    var room = client.channels.get(config.apply);//  my code right
-    var does = async function(some) {
-      if (some == "nope") return awaited.delete();
-      var data = new Discord.RichEmbed()
-      .setColor("#36393e")
-      .setThumbnail(message.author.displayAvatarURL)
-      .setAuthor(`${message.author.tag}`)
-      .setTimestamp()
-      .setFooter(`Applied At;`,client.user.displayAvatarURL)
-      .setDescription(`:sparkles: | ${message.author} \n**Name;** ${name}\n**Age;** ${age}\n**Country;** ${country}\n\`\`\`UserID; ${message.author.id}\`\`\``);
-      var mes = await room.send(data);
-      awaited.edit({embed: data
-          .setDescription(`Your apply will accepted soon , thanks for apply!`)
-      });
-      awaited.clearReactions();
-      createColl(mes.id);
-    };// she3aEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-    var createColl = async function(amessage) {
-      amessage = await room.fetchMessage(amessage);  
-      var sfil = (reaction,user) => reaction.emoji.name === `❎` && user.id != client.user.id;
-      var tfil = (reaction,user) => reaction.emoji.name === `✅` && user.id != client.user.id;
-      await amessage.react("✅");
-      await amessage.react("❎");
-   var truecoll = amessage.createReactionCollector(tfil,{
-                max: 1
-            });
-   var xcoll = amessage.createReactionCollector(sfil,{
-                max: 1
-            });
-    xcoll.on('collect', async resp => {
-      amessage.delete();// she3a
-      message.author.send(`You have been canceled by staff , your idenity card is not imporved`);// arabic devs , u know . arabic devs , you don't want to be like them . be better!
-    });
-    truecoll.on('collect', async resp => {
-    amessage.delete();
-    database[message.author.id] = {status:true,name: name, age: age,country: country};// she3a
-    var us = database[message.author.id];
-    var idenity = new Discord.RichEmbed()// she3a
-    .setColor("#36393e")
-    .setTimestamp()
-    .setFooter(client.user.username,client.user.displayAvatarURL)
-    .setThumbnail(message.author.displayAvatarURL)
-    .setDescription(`**${message.author} | :leaves: \nTake your idenity card**\`\`\`apache\nName; ${us.name}\nAge; ${us.age}\nCountry; ${us.country}\`\`\``);
-    message.author.send(`You Have Been accepted by staff , oho take ur idenity-card ;`);//i don't give a shit. 
-    message.author.send(idenity);
-    
-    });// she3a
-    };
-}).on('message', message => {
-    if (!message.content.startsWith(config.prefix + "idenity")) return undefined;
-    //Alpha wasn't good.
-     var user = message.mentions.users.first() || message.author;
-     user.data = database[user.id];
-     if (!user.data) return message.channel.send(`${user} dont have any data try ${config.prefix}register`);
-     var data = new Discord.RichEmbed()
-     .setColor("#36393e")
-     .setTimestamp()
-     .setThumbnail(user.displayAvatarURL)
-     .setFooter(client.user.username,client.user.displayAvatarURL)
-     .setDescription(`** ${user} idenity card; ** | :sparkles:\n\`\`\`apache\nName; ${user.data.name}\nAge; ${user.data.age}\nCountry; ${user.data.country}\`\`\``);
-     message.channel.send(data);
-     // end d; lol my code like notes , don't care. 
+    if (!message.guild) return;
+    if (!account[message.author.id]) {
+        account[message.author.id] = {
+            reg: false,
+            name: 'nothing'
+        };
+    }
+    if (message.content === `${prefix}register`) {
+        if (account[message.author.id].reg === true) return message.channel.send('❌ | لديك حساب مٌسجل بالفعل...');
+        if (message.author.bot) return;
+        const args = message.content.split(' ').slice(1);
+        if (!args[0]) return message.channel.send('❌ | أدخل إسم للتسجيل به.');
+        if (args[0]) {
+            account[message.author.id].reg = true;
+            account[message.author.id].name = args;
+            await saveChanges();
+            message.channel.send('You have registred your account !');
+        }
+    } else if (message.content === `${prefix}pingo`) { // Example on usage | مثال على الأوامر :
+        if (account[message.author.id].reg === false) return message.channel.send('❌ | يجب أن تكون مٌسجل لإستخدام هذا الأمر');
+        message.channel.send('PONG');
+    }
 });
+
+function saveChanges() {
+    return fs.writeFile('./account.json', JSON.stringify(account), error => {
+        if (error) console.log(error);
+    });
+}
 
 client.on('ready', () => {console.log(`Logged in as ${client.user.tag}!`);});
 
