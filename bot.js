b72a40d133462eeaf1a5729d3f73841a28da5050
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-13 04:09:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,54 +2,26 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
-const { Util } = require('discord.js');
+const { Client, Util } = require('discord.js');
 const Enmap = require('enmap');
 const ytScraper = require("yt-scraper")
 const prefix = "@"
 
 client.on('ready', () => {console.log(`Logged in as ${client.user.tag}!`);});
 
-const { Client, RichEmbed, Collection } = require('discord.js');
-const chalk = require('chalk');
-const hero = new Client();
-const auth = require('./auth.json');
+client.on("message", message => {
 
-hero.commands = new Collection();
-if(auth.token.toLowerCase() === 'heroku') {
-    hero.login(process.env.BOT_TOKEN);
-} else {
-    hero.login(auth.token);
-}
-
-fs.readdir('./Commands',function(err, files) {
-    if(err) console.log(err);
-    let cmds = files.filter(r => r.split('.').pop() === 'js');
-    if(cmds.length <= 0) return console.log(`# Broadcast: ${chalk.red("0 Commands to load.")}`);
-    cmds.forEach(r => {
-        let cmd = require(`./Commands/${r}`);
-        try {
-            hero.commands.set(cmd.help.name, cmd);
-            console.log(`# Broadcast: ${chalk.green(`Successfully loaded`)} ${chalk.bold.gray(r)}`);
-        } catch(e) {
-            if(e) return console.log(`# Broadcast: ${chalk.red(e.message + ".")}`);
-        }
-    });
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//copyright tex
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
 });
-
-hero.on('message',async message => {
-    if(message.author.bot || message.channel.type === 'dm') return;
-    let args = message.content.split(" ");
-    if(!message.content.startsWith(auth.prefix));
-    let cmd = hero.commands.get(args[0].slice(auth.prefix.length));
-    let emojis = {
-      yes: `${hero.guilds.get(auth.emojisId).emojis.find(r => r.name === "Yes")}`,
-      wrong: `${hero.guilds.get(auth.emojisId).emojis.find(r => r.name === "Wrong")}`,
-      settings: `${hero.guilds.get(auth.emojisId).emojis.find(r => r.name === "Settings")}`,
-      warn: `${hero.guilds.get(auth.emojisId).emojis.find(r => r.name === "Warn")}`
-    };
-    if(cmd) cmd.run(hero, message, args.slice(1), RichEmbed, emojis);
-});
-
 
 client.on('message', message => {
     if (message.content.startsWith("@hack")) {
