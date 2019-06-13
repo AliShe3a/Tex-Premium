f084841cabd2c87f7346e9ab7108c8cbfa90d4ef
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-13 03:50:12 +0200

    Update reload.js

--- Commands/reload.js
+++ Commands/reload.js
@@ -1,4 +1,4 @@
-const auth = require('../auth.json');
+const auth = require('./auth.json');
 const fs = require('fs');
 exports.run = async (hero, message, args, RichEmbed, emojis) => {
     if(!auth.admins.includes(message.author.id)) return message.channel.send(new RichEmbed()
