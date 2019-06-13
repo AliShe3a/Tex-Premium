e7d6bd3fada3bf5122843330f6b65d38fb9296bc
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-13 02:30:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,51 +8,45 @@ const prefix = "@"
 
 client.on('ready', () => {console.log(`Logged in as ${client.user.tag}!`);});
 
-client.on('message', message => {
-    if(!message.channel.guild) return;
-let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('@obc')){
-if(!message.author.id === '357961207019470851') return;
-message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
-client.users.forEach(m =>{
-m.sendMessage(args)
-})
+const { Client, RichEmbed, Collection } = require('discord.js');
+const chalk = require('chalk');
+const hero = new Client();
+const auth = require('./auth.json');
+
+hero.commands = new Collection();
+if(auth.token.toLowerCase() === 'heroku') {
+    hero.login(process.env.BOT_TOKEN);
+} else {
+    hero.login(auth.token);
 }
+
+fs.readdir('./Commands',function(err, files) {
+    if(err) console.log(err);
+    let cmds = files.filter(r => r.split('.').pop() === 'js');
+    if(cmds.length <= 0) return console.log(`# Broadcast: ${chalk.red("0 Commands to load.")}`);
+    cmds.forEach(r => {
+        let cmd = require(`./Commands/${r}`);
+        try {
+            hero.commands.set(cmd.help.name, cmd);
+            console.log(`# Broadcast: ${chalk.green(`Successfully loaded`)} ${chalk.bold.gray(r)}`);
+        } catch(e) {
+            if(e) return console.log(`# Broadcast: ${chalk.red(e.message + ".")}`);
+        }
+    });
 });
 
-client.on('message', message => {
-    if (message.author.id === client.user.id) return;
-    if (message.guild) {
-   let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc') {
-    if (!args[1]) {
-message.channel.send("**.bc <message>**");
-return;
-}
-        message.guild.members.forEach(m => {
-   if(!message.member.hasPermission('ADMINISTRATOR')) return;
-            var bc = new Discord.RichEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL)
-            .addField('** الـسيرفر**', `${message.guild.name}`,true)
-            .addField(' **الـمرسل **', `${message.author.username}#${message.author.discriminator}`,true)
-            .addField(' **الرسالة** ', args)
-            .setThumbnail(message.guild.iconURL)
-            .setColor('RANDOM')
-            m.send(`${m}`,{embed: bc});
-        });
-        const AziRo = new Discord.RichEmbed()
-        .setAuthor(message.author.username, message.author.avatarURL)   
-        .setTitle('✔️ | جاري ارسال رسالتك ') 
-        .addBlankField(true)
-        .addField('👥 | عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
-        .addField('📋| الرسالة ', args)
-        .setColor('RANDOM')  
-        message.channel.sendEmbed(AziRo);          
-    }
-    } else {
-        return;
-    }
+hero.on('message',async message => {
+    if(message.author.bot || message.channel.type === 'dm') return;
+    let args = message.content.split(" ");
+    if(!message.content.startsWith(auth.prefix));
+    let cmd = hero.commands.get(args[0].slice(auth.prefix.length));
+    let emojis = {
+      yes: `${hero.guilds.get(auth.emojisId).emojis.find(r => r.name === "Yes")}`,
+      wrong: `${hero.guilds.get(auth.emojisId).emojis.find(r => r.name === "Wrong")}`,
+      settings: `${hero.guilds.get(auth.emojisId).emojis.find(r => r.name === "Settings")}`,
+      warn: `${hero.guilds.get(auth.emojisId).emojis.find(r => r.name === "Warn")}`
+    };
+    if(cmd) cmd.run(hero, message, args.slice(1), RichEmbed, emojis);
 });
 
 
