806fa0ab88521193274b73f273c671417aff5e7b
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-13 02:44:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
     
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const fs = require('fs');
 const { Client, Util } = require('discord.js');
 const Enmap = require('enmap');
