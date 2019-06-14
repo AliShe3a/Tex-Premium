9cf7b3e1a7ac293e1e62d2eeec7504d4071ab60b
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-14 21:15:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -951,5 +951,17 @@ client.on("reachLimit", (limit)=> {
   });
 });
 
+client.on('message', message => {
+    if (message.content === "@pbot") {
+           if(!message.channel.guild) return message.reply('** This command only for servers **');
+    let embed = new Discord.RichEmbed()
+ .setColor('RANDOM')
+ .addField("**اسم السيرفر**", message.guild.name)
+ .addField("**عدد السيرفرات الي فيها البوت:**" , client.guilds.size)
+ .addField("**المستخدمين:**", client.users.size)
+ .addField("**قنوات:**", client.channels.size)
+message.channel.sendEmbed(embed);
+   }
+});
 
 client.login("NTg3NTk1MDc3Njk0NzgzNDg4.XP43Zg.rDI7D-W3OG_YAjIv5cQiog6MBBk");
