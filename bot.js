9da542387592a6ab10e53c8f09d11e2279156a6b
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-20 08:32:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,24 @@ const { Client, Util } = require('discord.js');
 const Enmap = require('enmap');
 const ytScraper = require("yt-scraper")
 
+    client.on('message', message => {
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+      
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'csay')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+        
+       
+      });
+
 client.on('message', message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
