bbc545d91549fdb72331d843d1a647e43f35d1c8
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-20 08:10:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,28 +47,6 @@ client.on('message', msg => {
        
       });
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-  
- 
-
-if (command == "esay") {
-    let say = new Discord.RichEmbed()
-    .setDescription(args.join("  "))
-    .setColor(0x23b2d6)
-    message.channel.sendEmbed(say);
-    message.delete();
-  }
-
-
-});
-
 client.on("message", msg => {
     const rooms = []
     msg.guild.channels.forEach(c => {
