bcfcc63a9dd1d3415a41cedda304d0a1b9d291b7
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-20 08:21:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -211,31 +211,8 @@ client.on('message', message => {
     });
 
 client.on('guildMemberAdd', member=> {
-    member.addRole(member.guild.roles.find("premium","member"));
+    member.addRole(member.guild.roles.find("name","member"));
     });
-
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-  
- 
-
-if (command == "say") {
-    let say = new Discord.RichEmbed()
-    .setDescription(args.join("  "))
-    .setColor(0x23b2d6)
-    message.channel.sendEmbed(say);
-    message.delete();
-  }
-
-
-});
 
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
