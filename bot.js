3dca5d1952990993d7766c56c581a556ac064d5c
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-27 21:45:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,27 @@ client.on('message', async message => {  //she3a
   }
 }) //MR.Egypt
 
+client.on("message", message => {
+ if (message.content === "@help") {
+	  message.channel.send('**تم ارسالك في الخاص** :mailbox_with_mail: ');
+  const embed = new Discord.RichEmbed() 
+      .setColor("#000000")
+      .setDescription(`
+	        اوامر الموسيقى 
+❖${prefix}play ~ لتشغيل أغنية برآبط أو بأسم
+❖${prefix}skip ~ لتجآوز الأغنية الحآلية
+❖${prefix}pause ~ إيقآف الأغنية مؤقتا
+❖${prefix}resume ~ لموآصلة الإغنية بعد إيقآفهآ مؤقتا
+❖${prefix}vol ~ لتغيير درجة الصوت 100 - 0
+❖${prefix}stop ~ لإخرآج البوت من الروم
+❖${prefix}np ~ لمعرفة الأغنية المشغلة حآليا
+❖${prefix}queue ~ لمعرفة قآئمة التشغيل
+ `)
+   message.author.sendEmbed(embed)
+    
+   }
+   });
+
  client.on('message', message => {
             if (message.author.id === client.user.id) return;
         if (message.guild) {
