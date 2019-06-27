fc40d96d66c804f31d48a934fc9718d2b0122b44
aligamer998 <36402790+aligamer998@users.noreply.github.com>
2019-06-27 20:24:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const Canvas = require('canvas')
 const client = new Discord.Client();
 const youtube = new YouTube("AIzaSyBPDpS-wRHLdJcxeeSqPTf7ixUUrwVp-fk");
 const fetchVideoInfo = require('youtube-info');
